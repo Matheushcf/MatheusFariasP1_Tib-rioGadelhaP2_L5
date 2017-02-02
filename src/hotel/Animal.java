8a02e5482179b479bb60295d14cdc20ed490e52a
Tibério Gadelha <tiberio.gomes@ccc.ufcg.edu.br>
2017-02-02 11:49:35 -0300

    adicionei o restante do projeto

--- src/hotel/Animal.java
+++ src/hotel/Animal.java
@@ -1,5 +1,15 @@
 package hotel;
 
 public class Animal {
-
+	private String nome;
+	private String tipo;
+	private int idade;
+	
+	public Animal(String nome, String tipo, int idade) {
+		this.nome = nome;
+		this.tipo = tipo;
+		this.idade = idade;
+	}
+	
+	
 }
